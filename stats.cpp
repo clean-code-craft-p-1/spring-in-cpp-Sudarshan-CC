e6ff59ea098071a9af0d420fd1fe79e5e6a6c78c
Sudarshan-CC <139843052+Sudarshan-CC@users.noreply.github.com>
2023-07-21 16:07:03 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,12 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<double>&vecValues ) {
     //Implement statistics here
+    double sum = 0;
+    for(int i = 0;i< vecValues.size();i++){
+        double sum = sum + vecValues.at[i];
+    }
+    Stats.average = sum/(vecValues.size());
+    Stats.Min = vecValues.[0];
+    Stats.Max = vecValues.[vecValues.size()];
 }
